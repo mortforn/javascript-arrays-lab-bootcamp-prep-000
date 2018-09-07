f2fa572662a9b90ab8c88404658730cdc68baa7c
Matthew Fine <mfine@wesleyan.edu>
2018-09-07 01:22:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Add your functions and code here
+
+const kittens = ['Milo', 'Otis', 'Garfield'];
+
+function appendKitten(name) {
+  var newKittens = kittens.concat(name);
+  return newKittens;
+} 
+
+function prependKitten(name) {
+  var newerKittens = kittens.concat(kittens.splice(0, name));
+  return newerKittens;
+}
+
+function removeLastKitten() {
+  var newKitten = kittens.slice(-1);
+  return newKitten;
+}
+
+function removeFirstKitten() {
+  var newKittens = kittens.concat(kittens.slice(0));
+  return newKittens;
+}
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+  return kittens;
+}
